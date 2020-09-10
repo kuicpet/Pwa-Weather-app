829e8bb635a7fc1306720556a54f748af7d8f4cd
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-10 23:15:39 +0100

    ft--install PWA added

---
+++ install.js
@@ -0,0 +1,31 @@
+window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
+
+let deferredInstallPrompt = null;
+const installButton = document.getElementById("butInstall");
+installButton.addEventListener("click", installPWA);
+
+function saveBeforeInstallPromptEvent(evt) {
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute('hidden');
+};
+
+function installPWA(evt) {
+    deferredInstallPrompt.prompt();
+    evt.srcElement.setAttribute("hidden", true);
+    
+    deferredInstallPrompt.userChoice
+        .then((choice) => {
+            if (choice.outcome === "accepted") {
+                console.log("User accepted the A2HS prompt", choice);
+            } else {
+                console.log("User dismissed the A2HS prompt", choice);
+            }
+            deferredInstallPrompt = null;
+        });
+}
+
+window.addEventListener("appInstalled", logAppInstalled);
+
+function logAppInstalled(evt) {
+    console.log("Weather App was installed. ", evt);
+}
